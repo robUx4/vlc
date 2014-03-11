4162ef604996c78eadb8cb80fa0128096e9e0db8
Jean-Baptiste Kempf <jb@videolan.org>
2014-03-12 00:11:26 +0100

    Accept the weird WinRT pathes

--- src/text/url.c
+++ src/text/url.c
@@ -176,6 +176,11 @@ char *vlc_path2uri (const char *path, const char *scheme)
         }
     }
     else
+#if VLC_WINSTORE_APP
+    if ((!strncmp(path, "file://{", 8) || !strncmp(path, "winrt://{", 9) || path[0] == '{') && strlen(path) > 32)
+        return strdup( path );
+    else
+#endif
     if (!strncmp (path, "\\\\", 2))
     {   /* Windows UNC paths */
         /* \\host\share\path -> file://host/share/path */
