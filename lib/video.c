017c3fbc7055ca8a7b78e7170feaaebdcddb75b7
Jean-Baptiste Kempf <jb@videolan.org>
2014-04-16 16:09:53 +0200

    Cheat for Windows Store subtitles

--- lib/video.c
+++ lib/video.c
@@ -350,7 +350,7 @@ int libvlc_video_set_subtitle_file( libvlc_media_player_t *p_mi,
 
     if( p_input_thread )
     {
-        if( !input_AddSubtitle( p_input_thread, psz_subtitle, true ) )
+        if( !input_AddSubtitle( p_input_thread, psz_subtitle, false) )
             b_ret = true;
         vlc_object_release( p_input_thread );
     }
