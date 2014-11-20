278ca74e6986e72bf12509d6f8f0320b3d703179
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2014-11-20 14:44:46 +0100

    rand: link properly to RoGetActivationFactory, MSVC can't use roapi.h from a C file

--- src/win32/rand.c
+++ src/win32/rand.c
@@ -30,8 +30,19 @@
 # define COBJMACROS
 # define INITGUID
 # include <winstring.h>
-# include <roapi.h>
 # include <windows.security.cryptography.h>
+# ifndef _MSC_VER /* roapi.h is a C++ include file that doesn't work with C files */
+#  include <roapi.h>
+# else /* _MSC_VER */
+DECLSPEC_IMPORT HRESULT WINAPI RoGetActivationFactory(_In_ HSTRING activatableClassId, _In_ REFIID iid,  _COM_Outptr_ void ** factory);
+typedef __x_ABI_CWindows_CSecurity_CCryptography_CICryptographicBufferStatics ICryptographicBufferStatics;
+typedef __x_ABI_CWindows_CStorage_CStreams_CIBuffer IBuffer;
+#   define ICryptographicBufferStatics_Release __x_ABI_CWindows_CSecurity_CCryptography_CICryptographicBufferStatics_Release
+#   define ICryptographicBufferStatics_CopyToByteArray __x_ABI_CWindows_CSecurity_CCryptography_CICryptographicBufferStatics_CopyToByteArray
+#   define IBuffer_Release __x_ABI_CWindows_CStorage_CStreams_CIBuffer_Release
+#   define ICryptographicBufferStatics_GenerateRandom __x_ABI_CWindows_CSecurity_CCryptography_CICryptographicBufferStatics_GenerateRandom
+DEFINE_GUID(IID_ICryptographicBufferStatics, 0x320b7e22, 0x3cb0, 0x4cdf, 0x86,0x63, 0x1d,0x28,0x91,0x00,0x65,0xeb);
+# endif /* _MSC_VER */
 #else
 # include <wincrypt.h>
 #endif
