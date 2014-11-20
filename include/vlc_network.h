8c40f92df6fb4ea0633b0fe3cbff6d1cb31cf06b
Jean-Baptiste Kempf <jb@videolan.org>
2014-11-20 13:12:05 +0100

    Fix gai_strerror on Unicode

--- include/vlc_network.h
+++ include/vlc_network.h
@@ -38,6 +38,13 @@
 #   include <vlc_winsock2.h>
 #   include <vlc_ws2tcpip.h>
 #   define net_errno (WSAGetLastError())
+#   undef gai_strerror
+#   ifdef UNICODE
+#       include <vlc_charset.h>
+#       define gai_strerror(a) FromWide(gai_strerrorW)
+#   else
+#       define gai_strerror gai_strerrorA
+#   endif
 
 struct iovec
 {
@@ -223,11 +230,6 @@ VLC_API ssize_t net_vaPrintf( vlc_object_t *p_this, int fd, const v_socket_t *, 
 #endif
 #ifndef AI_IDN
 # define AI_IDN 0 /* GNU/libc extension */
-#endif
-
-#ifdef _WIN32
-# undef gai_strerror
-# define gai_strerror gai_strerrorA
 #endif
 
 #ifdef __OS2__
