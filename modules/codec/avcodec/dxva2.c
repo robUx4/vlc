855bdd0e917a302610aaa6bbe22bd6b236acde7c
Steve Lhomme <robux4@videolabs.io>
2015-04-23 08:07:25 +0200

    Revert "DXVA2: lock the buffer pool when getting/releasing a buffer (v4)"
    This reverts commit 27f02d251962f5006486bd3910de5306b962f6a2.

--- modules/codec/avcodec/dxva2.c
+++ modules/codec/avcodec/dxva2.c
@@ -305,7 +305,6 @@ typedef struct {
     LPDIRECT3DSURFACE9 d3d;
     int                refcount;
     unsigned int       order;
-    vlc_mutex_t        *p_lock;
 } vlc_va_surface_t;
 
 #define VA_DXVA2_MAX_SURFACE_COUNT (64)
@@ -314,9 +313,6 @@ struct vlc_va_sys_t
     int          codec_id;
     int          width;
     int          height;
-
-    bool            b_thread_safe;
-    vlc_mutex_t     surface_lock;
 
     /* DLL */
     HINSTANCE             hd3d9_dll;
@@ -505,9 +501,6 @@ static int Get(vlc_va_t *va, picture_t *pic, uint8_t **data)
         return VLC_EGENERIC;
     }
 
-    if ( sys->b_thread_safe )
-        vlc_mutex_lock( &sys->surface_lock );
-
     /* Grab an unused surface, in case none are, try the oldest
      * XXX using the oldest is a workaround in case a problem happens with libavcodec */
     unsigned i, old;
@@ -529,10 +522,6 @@ static int Get(vlc_va_t *va, picture_t *pic, uint8_t **data)
     surface->order = sys->surface_order++;
     *data = (void *)surface->d3d;
     pic->context = surface;
-
-    if ( sys->b_thread_safe )
-        vlc_mutex_unlock( &sys->surface_lock );
-
     return VLC_SUCCESS;
 }
 
@@ -540,16 +529,11 @@ static void Release(void *opaque, uint8_t *data)
 {
     picture_t *pic = opaque;
     vlc_va_surface_t *surface = pic->context;
-    if ( surface->p_lock )
-        vlc_mutex_lock( surface->p_lock );
 
     surface->refcount--;
     pic->context = NULL;
     picture_Release(pic);
     (void) data;
-
-    if ( surface->p_lock )
-        vlc_mutex_unlock( surface->p_lock );
 }
 
 static void Close(vlc_va_t *va, AVCodecContext *ctx)
@@ -567,8 +551,6 @@ static void Close(vlc_va_t *va, AVCodecContext *ctx)
         FreeLibrary(sys->hdxva2_dll);
     if (sys->hd3d9_dll)
         FreeLibrary(sys->hd3d9_dll);
-    if ( sys->b_thread_safe )
-        vlc_mutex_destroy( &sys->surface_lock );
 
     free((char *)va->description);
     free(sys);
@@ -582,10 +564,6 @@ static int Open(vlc_va_t *va, AVCodecContext *ctx, const es_format_t *fmt)
 
     va->sys = sys;
     sys->codec_id = ctx->codec_id;
-
-    sys->b_thread_safe = ctx->thread_safe_callbacks;
-    if ( sys->b_thread_safe )
-        vlc_mutex_init( &sys->surface_lock );
 
     /* Load dll*/
     sys->hd3d9_dll = LoadLibrary(TEXT("D3D9.DLL"));
@@ -1031,8 +1009,6 @@ static int DxCreateVideoDecoder(vlc_va_t *va, int codec_id,
         surface->d3d = sys->hw_surface[i];
         surface->refcount = 0;
         surface->order = 0;
-        if ( sys->b_thread_safe )
-            surface->p_lock = &sys->surface_lock;
     }
     msg_Dbg(va, "IDirectXVideoAccelerationService_CreateSurface succeed with %d surfaces (%dx%d)",
             sys->surface_count, fmt->i_width, fmt->i_height);
