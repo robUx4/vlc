cbb49f035b056a0297341afed594c5f88aafa192
Steve Lhomme <robUx4@videolabs.io>
2015-04-29 09:20:10 +0200

    clean before merging

--- modules/video_output/msw/direct3d9.c
+++ modules/video_output/msw/direct3d9.c
@@ -857,7 +857,7 @@ static const d3d_format_t d3d_formats[] = {
     { "YV12",       MAKEFOURCC('Y','V','1','2'),    VLC_CODEC_I420,  0,0,0 },
     { "YV12",       MAKEFOURCC('Y','V','1','2'),    VLC_CODEC_J420,  0,0,0 },
     { "NV12",       MAKEFOURCC('N','V','1','2'),    VLC_CODEC_NV12,  0,0,0 },
-    { "DXVANV12",   MAKEFOURCC('N','V','1','2'),    VLC_CODEC_D3D9_OPAQUE,  0,0,0 },
+    { "DXA9",       MAKEFOURCC('N','V','1','2'),    VLC_CODEC_D3D9_OPAQUE,  0,0,0 },
     { "UYVY",       D3DFMT_UYVY,    VLC_CODEC_UYVY,  0,0,0 },
     { "YUY2",       D3DFMT_YUY2,    VLC_CODEC_YUYV,  0,0,0 },
     { "X8R8G8B8",   D3DFMT_X8R8G8B8,VLC_CODEC_RGB32, 0xff0000, 0x00ff00, 0x0000ff },
@@ -967,68 +967,70 @@ static int Direct3D9CreatePool(vout_display_t *vd, video_format_t *fmt)
     fmt->i_gmask  = d3dfmt->gmask;
     fmt->i_bmask  = d3dfmt->bmask;
 
+    if ( fmt->i_chroma == VLC_CODEC_D3D9_OPAQUE )
+        /* a DXA9 pool will be created when needed */
+        return VLC_SUCCESS;
+
     /* We create one picture.
      * It is useless to create more as we can't be used for direct rendering */
-    if ( fmt->i_chroma != VLC_CODEC_D3D9_OPAQUE )
-    {
-        /* Create a surface */
-        LPDIRECT3DSURFACE9 surface;
-        HRESULT hr = IDirect3DDevice9_CreateOffscreenPlainSurface(d3ddev,
-                                                                  fmt->i_visible_width,
-                                                                  fmt->i_visible_height,
-                                                                  d3dfmt->format,
-                                                                  D3DPOOL_DEFAULT,
-                                                                  &surface,
-                                                                  NULL);
-        if (FAILED(hr)) {
-            msg_Err(vd, "Failed to create picture surface. (hr=0x%lx)", hr);
-            return VLC_EGENERIC;
-        }
+
+    /* Create a surface */
+    LPDIRECT3DSURFACE9 surface;
+    HRESULT hr = IDirect3DDevice9_CreateOffscreenPlainSurface(d3ddev,
+                                                              fmt->i_visible_width,
+                                                              fmt->i_visible_height,
+                                                              d3dfmt->format,
+                                                              D3DPOOL_DEFAULT,
+                                                              &surface,
+                                                              NULL);
+    if (FAILED(hr)) {
+        msg_Err(vd, "Failed to create picture surface. (hr=0x%lx)", hr);
+        return VLC_EGENERIC;
+    }
 
 #ifndef NDEBUG
-        msg_Dbg(vd, "Direct3D created offscreen surface: %ix%i",
-                    fmt->i_visible_width, fmt->i_visible_height);
+    msg_Dbg(vd, "Direct3D created offscreen surface: %ix%i",
+                fmt->i_visible_width, fmt->i_visible_height);
 #endif
 
-        /* fill surface with black color */
-        IDirect3DDevice9_ColorFill(d3ddev, surface, NULL, D3DCOLOR_ARGB(0xFF, 0, 0, 0));
+    /* fill surface with black color */
+    IDirect3DDevice9_ColorFill(d3ddev, surface, NULL, D3DCOLOR_ARGB(0xFF, 0, 0, 0));
 
-        /* Create the associated picture */
-        picture_sys_t *picsys = malloc(sizeof(*picsys));
-        if (unlikely(picsys == NULL)) {
-            IDirect3DSurface9_Release(surface);
-            return VLC_ENOMEM;
-        }
-        picsys->surface = surface;
-        picsys->fallback = NULL;
+    /* Create the associated picture */
+    picture_sys_t *picsys = malloc(sizeof(*picsys));
+    if (unlikely(picsys == NULL)) {
+        IDirect3DSurface9_Release(surface);
+        return VLC_ENOMEM;
+    }
+    picsys->surface = surface;
+    picsys->fallback = NULL;
 
-        picture_resource_t resource = { .p_sys = picsys };
-        for (int i = 0; i < PICTURE_PLANE_MAX; i++)
-            resource.p[i].i_lines = fmt->i_visible_height / (i > 0 ? 2 : 1);
+    picture_resource_t resource = { .p_sys = picsys };
+    for (int i = 0; i < PICTURE_PLANE_MAX; i++)
+        resource.p[i].i_lines = fmt->i_visible_height / (i > 0 ? 2 : 1);
 
-        picture_t *picture = picture_NewFromResource(fmt, &resource);
-        if (!picture) {
-            msg_Err(vd, "Failed to create a picture from resources.");
-            IDirect3DSurface9_Release(surface);
-            free(picsys);
-            return VLC_ENOMEM;
-        }
-        sys->picsys = picsys;
+    picture_t *picture = picture_NewFromResource(fmt, &resource);
+    if (!picture) {
+        msg_Err(vd, "Failed to create a picture from resources.");
+        IDirect3DSurface9_Release(surface);
+        free(picsys);
+        return VLC_ENOMEM;
+    }
+    sys->picsys = picsys;
 
-        /* Wrap it into a picture pool */
-        picture_pool_configuration_t pool_cfg;
-        memset(&pool_cfg, 0, sizeof(pool_cfg));
-        pool_cfg.picture_count = 1;
-        pool_cfg.picture       = &picture;
-        pool_cfg.lock      = Direct3D9LockSurface;
-        pool_cfg.unlock    = Direct3D9UnlockSurface;
+    /* Wrap it into a picture pool */
+    picture_pool_configuration_t pool_cfg;
+    memset(&pool_cfg, 0, sizeof(pool_cfg));
+    pool_cfg.picture_count = 1;
+    pool_cfg.picture       = &picture;
+    pool_cfg.lock      = Direct3D9LockSurface;
+    pool_cfg.unlock    = Direct3D9UnlockSurface;
 
-        sys->pool = picture_pool_NewExtended(&pool_cfg);
-        if (!sys->pool) {
-            picture_Release(picture);
-            IDirect3DSurface9_Release(surface);
-            return VLC_ENOMEM;
-        }
+    sys->pool = picture_pool_NewExtended(&pool_cfg);
+    if (!sys->pool) {
+        picture_Release(picture);
+        IDirect3DSurface9_Release(surface);
+        return VLC_ENOMEM;
     }
     return VLC_SUCCESS;
 }
