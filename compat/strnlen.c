ff0c112221a76c5afd15bfb3e7367f32c60e8b2e
Steve Lhomme <robux4@gmail.com>
2015-06-25 13:59:30 +0200

    compat: if HAVE_STRLEN is forced we don't need to define the function

--- compat/strnlen.c
+++ compat/strnlen.c
@@ -24,8 +24,10 @@
 
 #include <string.h>
 
+#ifndef HAVE_STRNLEN
 size_t strnlen (const char *str, size_t max)
 {
     const char *end = memchr (str, 0, max);
     return end ? (size_t)(end - str) : max;
 }
+#endif
