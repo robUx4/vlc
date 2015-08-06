9fcd91bcd4b004621217018fac3d2f71cbd3443a
Steve Lhomme <robUx4@gmail.com>
2015-08-06 16:18:10 +0200

    fix some header definitions

--- include/vlc_fixups.h
+++ include/vlc_fixups.h
@@ -152,7 +152,7 @@ int ffsll(unsigned long long);
 void *memrchr(const void *, int, size_t);
 #endif
 
-#ifndef HAVE_STRCASECMP
+#if !defined(HAVE_STRCASECMP) && !defined(strcasecmp)
 int strcasecmp (const char *, const char *);
 #endif
 
@@ -160,7 +160,9 @@ int strcasecmp (const char *, const char *);
 char *strcasestr (const char *, const char *);
 #endif
 
-#ifndef HAVE_STRDUP
+#if defined(_MSC_VER) && _MSC_VER >= 1900
+#define strdup(x) _strdup(x)
+#elif !defined(HAVE_STRDUP)
 char *strdup (const char *);
 #endif
 
@@ -208,7 +210,7 @@ lldiv_t lldiv (long long, long long);
 #endif
 
 #ifndef HAVE_STRTOF
-#ifndef __ANDROID__
+#if !defined(__ANDROID__) && defined(_MSC_VER) && _MSC_VER < 1900
 float strtof (const char *, char **);
 #endif
 #endif
@@ -485,7 +487,7 @@ void freeaddrinfo (struct addrinfo *res);
 
 /* math.h */
 
-#ifndef HAVE_NANF
+#if !defined(HAVE_NANF) && defined(_MSC_VER) && _MSC_VER < 1900
 #define nanf(tagp) NAN
 #endif
 
