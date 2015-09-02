e9af62192578169a74d6536401363aa1bb89410a
Steve Lhomme <robUx4@gmail.com>
2015-09-02 18:02:51 +0200

    avcodec: reduce the amount of extra decoding buffers when using multithread

--- modules/codec/avcodec/video.c
+++ modules/codec/avcodec/video.c
@@ -451,7 +451,7 @@ int InitVideoDec( decoder_t *p_dec, AVCodecContext *p_context,
     }
 
     if( p_context->thread_type & FF_THREAD_FRAME )
-        p_dec->i_extra_picture_buffers = 2 * p_context->thread_count;
+        p_dec->i_extra_picture_buffers = p_context->thread_count - 1;
 #endif
 
     /* ***** misc init ***** */
