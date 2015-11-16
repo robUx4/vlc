29f4d2ab843116b8f838547da0cece31c5605811
Steve Lhomme <robux4@videolabs.io>
2015-11-16 10:35:18 +0100

    more flush

--- modules/stream_out/standard.c
+++ modules/stream_out/standard.c
@@ -411,6 +411,7 @@ static int Open( vlc_object_t *p_this )
     p_stream->pf_add    = Add;
     p_stream->pf_del    = Del;
     p_stream->pf_send   = Send;
+    p_stream->pf_flush  = Flush;
     if( !sout_AccessOutCanControlPace( p_access ) )
         p_stream->pace_nocontrol = true;
 
