97ab543b5ad111250eb19a2c1e6dba63ba0ae4b7
Steve Lhomme <robux4@videolabs.io>
2015-12-04 11:24:13 +0100

    services_discovery:chromecast: call microdns to get the list of available Chromecast devices

--- modules/services_discovery/chromecast.c
+++ modules/services_discovery/chromecast.c
@@ -31,8 +31,9 @@
 
 static const char MDNS_CHROMECAST[] = "._googlecast._tcp.local";
 
-static int Open ( vlc_object_t * );
+static int Open( vlc_object_t * );
 static void Close( vlc_object_t * );
+static void *Run( void *p_sd );
 
 VLC_SD_PROBE_HELPER("chromecast", "Chromecasts", SD_CAT_RENDERER)
 
@@ -55,6 +56,7 @@ vlc_module_end ()
 
 struct services_discovery_sys_t
 {
+    vlc_thread_t thread;
     struct mdns_ctx *microdns_ctx;
 };
 
@@ -70,15 +72,22 @@ static int Open (vlc_object_t *obj)
     if( !p_sys )
         return VLC_ENOMEM;
 
-    if (mdns_init(&p_sys->microdns_ctx, MDNS_ADDR_IPV4, MDNS_PORT) >= 0)
+    int err;
+    if (( err = mdns_init(&p_sys->microdns_ctx, MDNS_ADDR_IPV4, MDNS_PORT)) < 0)
     {
+        msg_Err( obj, "Can't initialize microdns:%d", err );
         goto error;
     }
 
-    // TODO services_discovery_AddItem (sd, item, _("Local drives"));
+    if ( vlc_clone( &p_sys->thread, Run, obj, VLC_THREAD_PRIORITY_LOW) )
+    {
+        msg_Err(obj, "Can't run the lookup thread");
+        goto error;
+    }
 
     return VLC_SUCCESS;
 error:
+    mdns_cleanup( p_sys->microdns_ctx );
     free( p_sys );
     return VLC_EGENERIC;
 }
@@ -91,7 +100,85 @@ static void Close( vlc_object_t *p_this )
     services_discovery_t *p_sd = ( services_discovery_t* )p_this;
     services_discovery_sys_t *p_sys = p_sd->p_sys;
 
-    mdns_cleanup(p_sys->microdns_ctx);
+    vlc_cancel( p_sys->thread );
+    vlc_join( p_sys->thread, NULL );
+
+    mdns_cleanup( p_sys->microdns_ctx );
 
     free( p_sys );
+}
+
+static void new_entry_callback( void *p_this, int i_status, const struct rr_entry *p_entry )
+{
+    services_discovery_t *p_sd = ( services_discovery_t* )p_this;
+
+    if (i_status < 0)
+    {
+        char err_str[128];
+        if (mdns_strerror(i_status, err_str, sizeof(err_str)) == 0)
+        {
+            msg_Dbg(p_sd, "mDNS lookup error: %s", err_str);
+        }
+    }
+    else if (p_entry != NULL && p_entry->next != NULL)
+    {
+        if (strlen(p_entry->next->name) >= strlen(MDNS_CHROMECAST))
+        {
+            char *psz_device_name = strndup( p_entry->next->name, strlen(p_entry->next->name) - strlen(MDNS_CHROMECAST) );
+            char *psz_service_name = strdup( p_entry->next->name + strlen(psz_device_name) );
+            if (!strcmp(psz_service_name, MDNS_CHROMECAST))
+            {
+                /* this is a Chromecast device */
+                char *deviceIP = NULL;
+                uint16_t devicePort = 0;
+                while (p_entry != NULL)
+                {
+                    if (p_entry->type == RR_A)
+                        deviceIP = strdup( p_entry->data.A.addr_str );
+                    else if (p_entry->type == RR_AAAA)
+                        deviceIP = strdup( p_entry->data.AAAA.addr_str );
+                    else if (p_entry->type == RR_SRV)
+                        devicePort = p_entry->data.SRV.port;
+                    p_entry = p_entry->next;
+                }
+                msg_Dbg(p_sd, "Found Chromecast '%s' %s:%d", psz_device_name, deviceIP, devicePort);
+
+                // TODO services_discovery_AddItem (sd, item, _("Local drives"));
+
+                free( deviceIP );
+            }
+            free( psz_device_name );
+            free( psz_service_name );
+        }
+    }
+}
+
+static bool should_stop_callback( void *p_this )
+{
+    services_discovery_t *p_sd = ( services_discovery_t* )p_this;
+    services_discovery_sys_t *p_sys = p_sd->p_sys;
+
+    vlc_testcancel();
+
+    return false;
+}
+
+void *Run( void *p_this )
+{
+    services_discovery_t *p_sd = ( services_discovery_t* )p_this;
+    services_discovery_sys_t *p_sys = p_sd->p_sys;
+
+    int err;
+    //int canc = vlc_savecancel();
+    if (( err = mdns_listen( p_sys->microdns_ctx, MDNS_CHROMECAST+1, 60, &should_stop_callback, new_entry_callback, p_sd )) < 0)
+    {
+        char err_str[128];
+        if (mdns_strerror(err, err_str, sizeof(err_str)) == 0)
+            msg_Err( p_sd, "Failed to look for the target Name: %s", err_str );
+        goto done;
+    }
+
+done:
+    //vlc_restorecancel(canc);
+    return NULL;
 }
