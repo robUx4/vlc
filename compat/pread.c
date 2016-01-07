4a5427a77c88f65f7d6883127f5b353b39755a73
Steve Lhomme <robux4@videolabs.io>
2016-01-07 14:26:19 +0100

    compat: add pread
    # Conflicts:
    #	configure.ac

---
+++ compat/pread.c
@@ -0,0 +1,53 @@
+/*****************************************************************************
+ * pread.c: POSIX C pread() replacement
+ *****************************************************************************
+ * Copyright © 2016 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#ifdef _WIN32
+#include <windows.h>
+
+ssize_t pread (int fd, void *buf, size_t count, off_t offset)
+{
+    HANDLE handle = (HANDLE)(intptr_t)_get_osfhandle (fd);
+    if (handle == INVALID_HANDLE_VALUE)
+        return -1;
+
+    OVERLAPPED olap; olap.Offset = offset; olap.OffsetHigh = (offset >> 32);
+    DWORD written;
+    /* This braindead API will override the file pointer even if we specify
+     * an explicit read offset... So do not expect this to mix well with
+     * regular read() calls. */
+    if (ReadFile (handle, buf, count, &written, &olap))
+        return written;
+    return -1;
+}
+#else
+#include <io.h>
+
+ssize_t pread (int fd, void *buf, size_t count, off_t offset)
+{
+    if ( lseek (fd, offset, SEEK_SET) != offset ) {
+        return -1;
+    }
+    return read( fd, buf, count );
+}
+#endif
--- src/misc/block.c
+++ src/misc/block.c
@@ -377,27 +377,6 @@ block_t *block_shm_Alloc (void *addr, size_t length)
 #endif
 
 
-#ifdef _WIN32
-# include <io.h>
-
-static
-ssize_t pread (int fd, void *buf, size_t count, off_t offset)
-{
-    HANDLE handle = (HANDLE)(intptr_t)_get_osfhandle (fd);
-    if (handle == INVALID_HANDLE_VALUE)
-        return -1;
-
-    OVERLAPPED olap; olap.Offset = offset; olap.OffsetHigh = (offset >> 32);
-    DWORD written;
-    /* This braindead API will override the file pointer even if we specify
-     * an explicit read offset... So do not expect this to mix well with
-     * regular read() calls. */
-    if (ReadFile (handle, buf, count, &written, &olap))
-        return written;
-    return -1;
-}
-#endif
-
 /**
  * Loads a file into a block of memory through a file descriptor.
  * If possible a private file mapping is created. Otherwise, the file is read
