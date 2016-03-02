d3bfe427406f3cd2d973aa9639ae7a2dad0e7468
Thomas Guillem <thomas@gllm.fr>
2016-03-02 11:37:33 +0100

    input: request a vlc_renderer when needed

--- src/input/input.c
+++ src/input/input.c
@@ -324,6 +324,7 @@ static input_thread_t *Create( vlc_object_t *p_parent, input_item_t *p_item,
     TAB_INIT( p_input->p->i_attachment, p_input->p->attachment );
     p_input->p->attachment_demux = NULL;
     p_input->p->p_sout   = NULL;
+    p_input->p->p_renderer = NULL;
     p_input->p->b_out_pace_control = false;
 
     vlc_gc_incref( p_item ); /* Released in Destructor() */
@@ -814,18 +815,38 @@ static int InitSout( input_thread_t * p_input )
 {
     if( p_input->b_preparsing )
         return VLC_SUCCESS;
+    char *psz;
+    const bool b_is_cmd = !strncasecmp( p_input->p->p_item->psz_uri, "vlc:", 4 );
+
+    if( !b_is_cmd && ( psz = var_InheritString( p_input, "renderer" ) ) )
+    {
+        p_input->p->p_renderer =
+            input_resource_RequestRenderer( p_input->p->p_resource, NULL, psz );
+        free( psz );
+        if( !p_input->p->p_renderer )
+        {
+            input_ChangeState( p_input, ERROR_S );
+            msg_Err( p_input, "cannot start renderer instance, " \
+                              "aborting" );
+            return VLC_EGENERIC;
+        }
+    }
+    else
+    {
+        p_input->p->p_renderer = input_resource_RequestRenderer( p_input->p->p_resource,
+                                                                 p_input->p->p_renderer, NULL );
+    }
 
     /* Find a usable sout and attach it to p_input */
-    char *psz = var_GetNonEmptyString( p_input, "sout" );
-    if( psz && strncasecmp( p_input->p->p_item->psz_uri, "vlc:", 4 ) )
+    if( !b_is_cmd && ( psz = var_GetNonEmptyString( p_input, "sout" ) ) )
     {
         p_input->p->p_sout  = input_resource_RequestSout( p_input->p->p_resource, NULL, psz );
+        free( psz );
         if( !p_input->p->p_sout )
         {
             input_ChangeState( p_input, ERROR_S );
             msg_Err( p_input, "cannot start stream output instance, " \
                               "aborting" );
-            free( psz );
             return VLC_EGENERIC;
         }
         if( libvlc_stats( p_input ) )
@@ -840,7 +861,6 @@ static int InitSout( input_thread_t * p_input )
         p_input->p->p_sout = input_resource_RequestSout( p_input->p->p_resource,
                                                          p_input->p->p_sout, NULL );
     }
-    free( psz );
 
     return VLC_SUCCESS;
 }
@@ -1250,6 +1270,9 @@ error:
         if( p_input->p->p_sout )
             p_input->p->p_sout = input_resource_RequestSout( p_input->p->p_resource,
                                          p_input->p->p_sout, NULL );
+        if( p_input->p->p_renderer )
+            p_input->p->p_renderer = input_resource_RequestRenderer( p_input->p->p_resource,
+                                                                     p_input->p->p_renderer, NULL );
         input_resource_SetInput( p_input->p->p_resource, NULL );
         if( p_input->p->p_resource_private )
             input_resource_Terminate( p_input->p->p_resource_private );
@@ -1287,6 +1310,7 @@ error:
     /* Mark them deleted */
     p_input->p->p_es_out = NULL;
     p_input->p->p_sout = NULL;
+    p_input->p->p_renderer = NULL;
 
     return VLC_EGENERIC;
 }
@@ -1365,6 +1389,8 @@ static void End( input_thread_t * p_input )
     /* */
     p_input->p->p_sout = input_resource_RequestSout( p_input->p->p_resource,
                                                      p_input->p->p_sout, NULL );
+    p_input->p->p_renderer = input_resource_RequestRenderer( p_input->p->p_resource,
+                                                             p_input->p->p_renderer, NULL );
     input_resource_SetInput( p_input->p->p_resource, NULL );
     if( p_input->p->p_resource_private )
         input_resource_Terminate( p_input->p->p_resource_private );
