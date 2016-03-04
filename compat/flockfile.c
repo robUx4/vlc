4ca8a96348fe6f26eba330ab09a39c6011f64f37
Steve Lhomme <robux4@videolabs.io>
2016-03-04 10:26:16 +0100

    compat:flockfile: fix linking with MinGW64

--- compat/flockfile.c
+++ compat/flockfile.c
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#define _CRTIMP
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
