6f8ae30d93c1ad081832fd0e3af53c94538fd207
Steve Lhomme <robux4@videolabs.io>
2016-06-07 16:50:24 +0200

    compat: add compatibility functions needed by some tests

---
+++ compat/mkdtemp.c
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ * mkdtemp.c: mkdtemp() emulation
+ *****************************************************************************
+ * Copyright © 2016 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+char *mkdtemp(char *path_template)
+{
+    (void) path_template;
+    return NULL;
+}
