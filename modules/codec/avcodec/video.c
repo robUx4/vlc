f8676be5b45372cb508f9a162410eea0ddccff22
Steve Lhomme <robux4@videolabs.io>
2016-06-08 17:40:34 +0200

    avcodec: enable thread hardware decoding with FFmpeg on Windows

--- modules/codec/avcodec/video.c
+++ modules/codec/avcodec/video.c
@@ -1281,7 +1281,7 @@ static enum PixelFormat ffmpeg_GetFormat( AVCodecContext *p_context,
     if (!can_hwaccel)
         return swfmt;
 
-#if (LIBAVCODEC_VERSION_MICRO >= 100) /* FFmpeg only */
+#if !defined(_WIN32) && (LIBAVCODEC_VERSION_MICRO >= 100) /* FFmpeg only */
     if (p_context->active_thread_type)
     {
         msg_Warn(p_dec, "thread type %d: disabling hardware acceleration",
