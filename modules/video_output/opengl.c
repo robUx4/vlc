540abe4b6a54cf214c28e22c8bd57858fe4b0791
Steve Lhomme <robux4@videolabs.io>
2016-09-19 17:06:13 +0200

    opengl: no need to restrict the radian values

--- modules/video_output/opengl.c
+++ modules/video_output/opengl.c
@@ -783,17 +783,9 @@ void vout_display_opengl_Delete(vout_display_opengl_t *vgl)
 
 void vout_display_opengl_SetViewpoint(vout_display_opengl_t *vgl, const vlc_viewpoint_t *p_viewpoint)
 {
-    const projection_mode proj_mode = vout_display_projection(vgl);
-
     vgl->f_teta = p_viewpoint->f_yaw - M_PI / 2;
-    if (vgl->f_teta < -M_PI)
-        vgl->f_teta = M_PI;
     vgl->f_phi = p_viewpoint->f_pitch;
-    if (vgl->f_phi < -M_PI / 2)
-        vgl->f_phi = -M_PI / 2;
     vgl->f_roll = p_viewpoint->f_roll;
-    if (vgl->f_roll < -M_PI / 2)
-        vgl->f_roll = -M_PI / 2;
 }
 
 picture_pool_t *vout_display_opengl_GetPool(vout_display_opengl_t *vgl, unsigned requested_count)
