6aacb74c0faac724d33b0c86fd79e875ba3a2f2d
Steve Lhomme <slhomme@matroska.org>
2016-11-24 10:56:06 +0100

    direct3d11: fix compilation with MSVC
    it doesn't like static in table parameters

--- modules/video_output/win32/direct3d11.c
+++ modules/video_output/win32/direct3d11.c
@@ -982,7 +982,7 @@ static HRESULT UpdateBackBuffer(vout_display_t *vd)
 }
 
 /* rotation around the Z axis */
-static void getZRotMatrix(float theta, FLOAT matrix[static 16])
+static void getZRotMatrix(float theta, FLOAT matrix[16])
 {
     float st, ct;
 
@@ -1000,7 +1000,7 @@ static void getZRotMatrix(float theta, FLOAT matrix[static 16])
 }
 
 /* rotation around the Y axis */
-static void getYRotMatrix(float theta, FLOAT matrix[static 16])
+static void getYRotMatrix(float theta, FLOAT matrix[16])
 {
     float st, ct;
 
@@ -1018,7 +1018,7 @@ static void getYRotMatrix(float theta, FLOAT matrix[static 16])
 }
 
 /* rotation around the X axis */
-static void getXRotMatrix(float phi, FLOAT matrix[static 16])
+static void getXRotMatrix(float phi, FLOAT matrix[16])
 {
     float sp, cp;
 
@@ -1035,7 +1035,7 @@ static void getXRotMatrix(float phi, FLOAT matrix[static 16])
     memcpy(matrix, m, sizeof(m));
 }
 
-static void getZoomMatrix(float zoom, FLOAT matrix[static 16]) {
+static void getZoomMatrix(float zoom, FLOAT matrix[16]) {
 
     const FLOAT m[] = {
         /* x   y     z     w */
@@ -1049,7 +1049,7 @@ static void getZoomMatrix(float zoom, FLOAT matrix[static 16]) {
 }
 
 /* perspective matrix see https://www.opengl.org/sdk/docs/man2/xhtml/gluPerspective.xml */
-static void getProjectionMatrix(float sar, float fovy, FLOAT matrix[static 16]) {
+static void getProjectionMatrix(float sar, float fovy, FLOAT matrix[16]) {
 
     float zFar  = 1000;
     float zNear = 0.01;
@@ -2454,7 +2454,7 @@ static void Direct3D11DestroyPool(vout_display_t *vd)
  * Vertex 0 should be assigned coordinates at index 2 from the
  * unrotated order and so on, thus yielding order: 2 3 0 1.
  */
-static void orientationVertexOrder(video_orientation_t orientation, int vertex_order[static 4])
+static void orientationVertexOrder(video_orientation_t orientation, int vertex_order[4])
 {
     switch (orientation) {
         case ORIENT_ROTATED_90:
