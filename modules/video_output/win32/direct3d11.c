10299a073102c9d3bc5c580db28b349c21b942ed
Steve Lhomme <robux4@videolabs.io>
2017-01-13 09:57:52 +0100

    direct3d11: TEXCOORD0 should be a float4

--- modules/video_output/win32/direct3d11.c
+++ modules/video_output/win32/direct3d11.c
@@ -168,13 +168,13 @@ static const char* globVertexShaderFlat = "\
   struct VS_INPUT\
   {\
     float4 Position   : POSITION;\
-    float2 Texture    : TEXCOORD0;\
+    float4 Texture    : TEXCOORD0;\
   };\
   \
   struct VS_OUTPUT\
   {\
     float4 Position   : SV_POSITION;\
-    float2 Texture    : TEXCOORD0;\
+    float4 Texture    : TEXCOORD0;\
   };\
   \
   VS_OUTPUT VS( VS_INPUT In )\
@@ -195,13 +195,13 @@ static const char* globVertexShaderProjection = "\
   struct VS_INPUT\
   {\
     float4 Position   : POSITION;\
-    float2 Texture    : TEXCOORD0;\
+    float4 Texture    : TEXCOORD0;\
   };\
   \
   struct VS_OUTPUT\
   {\
     float4 Position   : SV_POSITION;\
-    float2 Texture    : TEXCOORD0;\
+    float4 Texture    : TEXCOORD0;\
   };\
   \
   VS_OUTPUT VS( VS_INPUT In )\
@@ -239,7 +239,7 @@ static const char* globPixelShaderDefault = "\
   struct PS_INPUT\
   {\
     float4 Position   : SV_POSITION;\
-    float2 Texture    : TEXCOORD0;\
+    float4 Texture    : TEXCOORD0;\
   };\
   \
   float4 PS( PS_INPUT In ) : SV_TARGET\
@@ -255,6 +255,7 @@ static const char* globPixelShaderDefault = "\
   }\
 ";
 
+/* for NV12/P010 source */
 static const char *globPixelShaderBiplanarYUV_2RGB = "\
   cbuffer PS_CONSTANT_BUFFER : register(b0)\
   {\
@@ -276,7 +277,7 @@ static const char *globPixelShaderBiplanarYUV_2RGB = "\
   struct PS_INPUT\
   {\
     float4 Position   : SV_POSITION;\
-    float2 Texture    : TEXCOORD0;\
+    float4 Texture    : TEXCOORD0;\
   };\
   \
   float4 PS( PS_INPUT In ) : SV_TARGET\
@@ -314,7 +315,7 @@ static const char *globPixelShaderBiplanarYUYV_2RGB = "\
   struct PS_INPUT\
   {\
     float4 Position   : SV_POSITION;\
-    float2 Texture    : TEXCOORD0;\
+    float4 Texture    : TEXCOORD0;\
   };\
   \
   float4 PS( PS_INPUT In ) : SV_TARGET\
