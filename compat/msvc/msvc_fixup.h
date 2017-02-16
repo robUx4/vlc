780706ebd3ddeec6e675b06dede3f95e6e26b64b
Steve Lhomme <robux4@videolabs.io>
2017-02-16 14:19:49 +0100

    coremake: split the fixups specific to Clang and MSVC
    and mscver_fixup.h has the common stuff needed for both

--- compat/msvc/msvc_fixup.h
+++ compat/msvc/msvc_fixup.h
@@ -6,6 +6,8 @@
 #define WIN32_LEAN_AND_MEAN
 #endif /* WIN32_LEAN_AND_MEAN */
 
+#include "mscver_fixup.h"
+
 #if 0
 #ifndef NOUSER
 #define NOUSER /* avoid bogus pragma push/pop */
@@ -21,30 +23,15 @@ typedef struct tagMSG MSG, *LPMSG;
 #define __STDC_VERSION__ 199901L
 #endif
 
-#if defined(__clang__)
-#define _CRT_DECLARE_NONSTDC_NAMES 1
-#endif
-
 # if !defined(snprintf) && _MSC_VER < 1900
 #  define HAVE_SNPRINTF /* bogus autoconf detection using a define */
 #  define snprintf _snprintf
 # endif
-# ifndef snwprintf
-#  define HAVE_SNWPRINTF /* bogus autoconf detection using a define */
-#  define snwprintf _snwprintf
-# endif
-# if !defined(strdup) && (_MSC_VER < 1900 || defined(__clang__))
-//#  define HAVE_STRDUP /* bogus autoconf detection using a define */
+# if !defined(strdup) && _MSC_VER < 1900
+#  define HAVE_STRDUP /* bogus autoconf detection using a define */
 # define strdup _strdup
 #endif
-#define ssize_t  SSIZE_T
 
-/* force _FILE_OFFSET_BITS */
-#define _OFF_T_DEFINED
-typedef __int64 _off_t;
-typedef _off_t off_t;
-
-typedef unsigned int mode_t;
 //# define N_(x) x
 //# define _(x) x
 
@@ -60,93 +47,13 @@ typedef unsigned int mode_t;
 # define bcopy memcpy
 # define bcmp memcmp
 
-#if defined(__clang__) /* && !defined(__cplusplus) */
-# define restrict __restrict
-#else
-# define restrict 
-#endif
-
-// Stats part
-
-#define	_S_IFBLK	0x3000	/* Block: Is this ever set under w32? */
-
-#define _S_IFMT 0xF000
-#define _S_IFDIR 0x4000
-#define _S_IFCHR 0x2000
-#define _S_IFIFO 0x1000
-#define _S_IFREG 0x8000
-#define _S_IREAD 0x0100
-#define _S_IWRITE 0x0080
-#define _S_IEXEC 0x0040
-
-#define S_IFMT _S_IFMT
-#define S_IFDIR _S_IFDIR
-#define S_IFCHR _S_IFCHR
-#define S_IFREG _S_IFREG
-#define S_IREAD _S_IREAD
-#define S_IWRITE _S_IWRITE
-#define S_IEXEC _S_IEXEC
-#define	S_IFIFO		_S_IFIFO
-#define	S_IFBLK		_S_IFBLK
-
-#define	_S_IRWXU	(_S_IREAD | _S_IWRITE | _S_IEXEC)
-#define	_S_IXUSR	_S_IEXEC
-#define	_S_IWUSR	_S_IWRITE
-
-#define	S_IRWXU		_S_IRWXU
-#define	S_IXUSR		_S_IXUSR
-#define	S_IWUSR		_S_IWUSR
-#define	S_IRUSR		_S_IRUSR
-#define	_S_IRUSR	_S_IREAD
-
-#define O_RDONLY  _O_RDONLY
-#define O_WRONLY  _O_WRONLY
-#define O_CREAT   _O_CREAT
-#define O_TRUNC   _O_TRUNC
-#define O_BINARY  _O_BINARY
-#define O_APPEND  _O_APPEND
-#define O_EXCL    _O_EXCL
-#define O_RDWR    _O_RDWR
-#define O_TEXT    _O_TEXT
-#define O_NOINHERIT _O_NOINHERIT
-
-#if defined(__clang__)
-//#define stat _stat64
-//#define fstat(fd,out) _fstat64(fd,out)
-/* NO extern int stricmp(char const * s1, char const * s2); */
-//extern void* fdopen(int fd, char const* fmt);
-extern int read(int fd, void * buf, unsigned int bufcount);
-extern int write(int fd, void const * buf, unsigned int bufcount);
-extern int close(int fd);
-extern int setmode(int fd, int mode);
-extern int dup(int fd);
-/* NO extern void swab(char * b1, char * b2, int size); */
-extern long lseek(int fd, long offset, int origin);
-//extern int fileno(void* FILE);
-#endif
-
-# ifndef S_ISREG
-#  define S_ISREG(m) (((m) & S_IFMT) == S_IFREG)
-# endif
-
-#ifndef S_ISDIR
-# define S_ISDIR(mode)  (((mode) & S_IFMT) == S_IFDIR)
-#endif
+#define restrict 
 
 #define	S_ISFIFO(m)	(((m) & S_IFMT) == S_IFIFO)
 #define	S_ISCHR(m)	(((m) & S_IFMT) == S_IFCHR)
 #define	S_ISBLK(m)	(((m) & S_IFMT) == S_IFBLK)
 
 #define __func__ __FUNCDNAME__
-
-# if !defined(strcasecmp)
-# define HAVE_STRCASECMP /* bogus autoconf detection using a define */
-# define strcasecmp _stricmp
-#endif
-# if !defined(strncasecmp)
-#  define HAVE_STRNCASECMP /* bogus autoconf detection using a define */
-# define strncasecmp _strnicmp
-#endif
 # if !defined(vsnprintf) && !defined(__cplusplus) && _MSC_VER < 1900
 #  define HAVE_VSNPRINTF /* bogus autoconf detection using a define */
 #  define vsnprintf _vsnprintf
@@ -188,22 +95,7 @@ extern long lseek(int fd, long offset, int origin);
 #define _UNICODE
 #endif /* _UNICODE */
 
-#ifndef SSIZE_MAX
-#ifdef _WIN64
-#define SSIZE_MAX _I64_MAX
-#else
-#define SSIZE_MAX INT_MAX
-#endif
-#endif
-
 #define PATH_MAX MAX_PATH
-
-#if defined(__clang__)
-# define STATIC_ASSERT_CONCAT_(a, b) a##b
-# define STATIC_ASSERT_CONCAT(a, b) STATIC_ASSERT_CONCAT_(a, b)
-# define _Static_assert(x, s) extern char STATIC_ASSERT_CONCAT(static_assert_, __LINE__)[sizeof(struct { unsigned:-!(x); })]
-# define static_assert _Static_assert
-#endif /* __clang__ */
 
 /* avoid compilation issues in ARM */
 #define _ARM_WINAPI_PARTITION_DESKTOP_SDK_AVAILABLE 1
