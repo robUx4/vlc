73e1b727fd6dae533201c86da87046cd416308ce
Steve Lhomme <robux4@videolabs.io>
2017-04-05 12:01:07 +0200

    avcodec: encoder: fix sanity check for the frame rate
    We need to test both values for 0 or at least num so we don't result in a
    rational number with 0 divider.

--- modules/codec/avcodec/encoder.c
+++ modules/codec/avcodec/encoder.c
@@ -242,11 +242,17 @@ static void probe_video_frame_rate( encoder_t *p_enc, AVCodecContext *p_context,
 {
     /* if we don't have frame_rate.den, we are probing and just checking if we can find codec
      * so set fps to requested fps if asked by user or input fps is availabled */
-    p_context->time_base.num = p_enc->fmt_in.video.frame_rate.den ? p_enc->fmt_in.video.frame_rate.den : 1;
-
-    // MP4V doesn't like CLOCK_FREQ denominator in time_base, so use 1/25 as default for that
-    p_context->time_base.den = p_enc->fmt_in.video.frame_rate.den ? p_enc->fmt_in.video.frame_rate.num :
-                                  ( p_enc->fmt_out.i_codec == VLC_CODEC_MP4V ? 25 : CLOCK_FREQ );
+    if ( es_format_HasValidFrameRate( &p_enc->fmt_in ) )
+    {
+        p_context->time_base.num = p_enc->fmt_in.video.frame_rate.den;
+        p_context->time_base.den = p_enc->fmt_in.video.frame_rate.num;
+    }
+    else
+    {
+        // MP4V doesn't like CLOCK_FREQ denominator in time_base, so use 1/25 as default for that
+        p_context->time_base.num = 1;
+        p_context->time_base.den = p_enc->fmt_out.i_codec == VLC_CODEC_MP4V ? 25 : CLOCK_FREQ;
+    }
 
     msg_Dbg( p_enc, "Time base for probing set to %d/%d", p_context->time_base.num, p_context->time_base.den );
     if( p_codec->supported_framerates )
