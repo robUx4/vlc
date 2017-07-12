b3f36a69382b80c92a6c34fe56f527330145522c
Steve Lhomme <slhomme@matroska.org>
2017-07-12 11:20:16 +0200

    SQUASH direct3d11: fix compilation with MSVC
    it doesn't like static in table parameters

--- modules/video_output/win32/direct3d11.c
+++ modules/video_output/win32/direct3d11.c
@@ -2295,7 +2295,7 @@ static void Direct3D11DestroyPool(vout_display_t *vd)
  * Vertex 0 should be assigned coordinates at index 2 from the
  * unrotated order and so on, thus yielding order: 2 3 0 1.
  */
-static void orientationVertexOrder(video_orientation_t orientation, int vertex_order[static 4])
+static void orientationVertexOrder(video_orientation_t orientation, int vertex_order[4])
 {
     switch (orientation) {
         case ORIENT_ROTATED_90:
