b302a293bae4658fe9592882981bd4e28dc97dcb
Steve Lhomme <robux4@videolabs.io>
2017-10-20 09:35:58 +0200

    SQUASH fix if_nametoindex detection with MSVC

--- modules/services_discovery/sap.c
+++ modules/services_discovery/sap.c
@@ -57,10 +57,6 @@
 #ifdef HAVE_NET_IF_H
 #   include <net/if.h>
 #else
-# if _WIN32_WINNT <= 0x603
-#  undef WINAPI_FAMILY
-#  define WINAPI_FAMILY WINAPI_FAMILY_DESKTOP_APP
-# endif
 # include <iphlpapi.h>
 #endif
 
